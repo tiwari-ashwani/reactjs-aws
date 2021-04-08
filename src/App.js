c097837ef59900527d9e8eef16b9dd677654ad21
tiwari-ashwani <tiwariashwanik@gmail.com>
2021-04-08 22:58:35 +0200

    Refs changes

--- src/App.js
+++ src/App.js
@@ -19,12 +19,14 @@ import styles from './appStyles.module.css';
 import Form from './components/Form'
 import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
 
 class App extends Component{
   render(){
     return (
       <div className='App'>
-        <RefsDemo />
+        <FocusInput />
+        {/* <RefsDemo /> */}
         {/* <ParentComp /> */}
         {/* <FragmentDemo /> */}
         {/* <LifecycleMounting /><br /> */}
--- src/components/RefsDemo.js
+++ src/components/RefsDemo.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 
 class RefsDemo extends Component {
+
+    constructor(props){
+        super(props)
+        this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setCbRef = (element) => {
+            this.cbRef = element
+        }
+    }
+
+
+    componentDidMount(){
+        if(this.cbRef){
+            this.cbRef.focus()
+        }
+
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
+    }
+
+    clickHandler = () => {
+        alert(this.inputRef.current.value)
+    }
+
     render() {
         return (
             <div>
-                <input />
+                <input type='text' ref={this.inputRef}/><br />
+                <input type='text' ref={this.setCbRef}/><br />
+                <button onClick={this.clickHandler}>CLick Me</button>
             </div>
         )
     }
