eace60e9274a0cb3f7a2b6a0e25a35bd5c29fc7d
tiwari-ashwani <tiwariashwanik@gmail.com>
2021-04-30 10:11:39 +0200

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,73 +1,89 @@
-// import logo from './logo.svg';
-import './App.css';
 import React, { Component, useReducer } from 'react'
-import './appStyles.css'
-import CompA from './components/CompA';
-import CompB from './components/CompB';
-import CompC from './components/CompC';
-
-
-export const CountContext = React.createContext()
-
-const initialState = 0
-
-const reducer = (state, action) => {
-  switch (action) {
-    case 'increment':
-      return state + 1
-    case 'decrement':
-      return state - 1
-    case 'reset':
-      return initialState
-    default:
-      return state
-  }
-}
+import './App.css';
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
-  return (   
-    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
-      <div className="App">
-        Count = {count}<br />
-        <CompA /><br />
-        <CompB /><br />
-        <CompC /><br />
-      </div>
-    </CountContext.Provider>
-
-    //  { <h1 className='error'>Error Style</h1>
-    // <h1 className={styles.success}>Success Style</h1>
-    // <InlineStyling />
-    // <Stylesheet primary={true} />
-    // <NameList /> <br />
-    // <UserGreeting /> <br />
-    // <ParentComponent /><br />
-    // <Greet name="Mister" place="Old Gotham" />
-    // <Greet name="Bruce" place="Gotham" />
-    // <Greet name="Wayne" place="Old Gotham" />
-    // <GreetClass name="Mister" place="Old Gotham" />
-    // <GreetClass name="Bruce" place="Gotham" />
-    // <GreetClass name="Wayne" place="New Gotham" />
-    // <Hello />
-    // <Message /><br />
-    // <Counter /><br />
-    // <FunctionClick /><br />
-    // <ClassClick /><br />
-    // <EventBind /> } 
-  )
+    return (
+        <div className="App">
+        </div>
+    );
 }
+export default App;
+
+
+
+
+
+            // // import logo from './logo.svg';
+            // import './App.css';
+// import React, { Component, useReducer } from 'react'
+// import './appStyles.css'
+// import Form from './components/Form';
+// import CompB from './components/CompB';
+// import CompC from './components/CompC';
+// import FunctionForm from './components/FunctionForm';
+
+
+// // export const CountContext = React.createContext()
+
+// // const initialState = 0
+
+// // const reducer = (state, action) => {
+// //   switch (action) {
+// //     case 'increment':
+// //       return state + 1
+// //     case 'decrement':
+// //       return state - 1
+// //     case 'reset':
+// //       return initialState
+// //     default:
+// //       return state
+// //   }
+// // }
+
+// // function App() {
+// //   const [count, dispatch] = useReducer(reducer, initialState)
+// //   return (   
+// //     <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+// //       <div className="App">
+// //         Count = {count}<br />
+// //         <CompA /><br />
+// //         <CompB /><br />
+// //         <CompC /><br />
+// //       </div>
+// //     </CountContext.Provider>
+
+// //     //  { <h1 className='error'>Error Style</h1>
+// //     // <h1 className={styles.success}>Success Style</h1>
+// //     // <InlineStyling />
+// //     // <Stylesheet primary={true} />
+// //     // <NameList /> <br />
+// //     // <UserGreeting /> <br />
+// //     // <ParentComponent /><br />
+// //     // <Greet name="Mister" place="Old Gotham" />
+// //     // <Greet name="Bruce" place="Gotham" />
+// //     // <Greet name="Wayne" place="Old Gotham" />
+// //     // <GreetClass name="Mister" place="Old Gotham" />
+// //     // <GreetClass name="Bruce" place="Gotham" />
+// //     // <GreetClass name="Wayne" place="New Gotham" />
+// //     // <Hello />
+// //     // <Message /><br />
+// //     // <Counter /><br />
+// //     // <FunctionClick /><br />
+// //     // <ClassClick /><br />
+// //     // <EventBind /> } 
+// //   )
+// // }
 
 // class App extends Component {
 
 //   render() {
 //   return (
 //     <div className='App'>
-//       Count is :: {count}
 //       <br />
-//       <CompA />
-//       <CompB />
-//       <CompC />
+//       <FunctionForm />
+//       {/* <CompA /> */}
+//       {/* <CompB /> */}
+//       {/* <CompC /> */}
 
 //       {/* <CounterThree /> */}
 //       {/* <CounterTwo /> */}
@@ -137,6 +153,6 @@ function App() {
 //     </div>
 //   )
 // }
-//}
+// }
 
-export default App;
+// export default App;
